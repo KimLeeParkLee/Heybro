0d3ed3f2fede252254921bcac5e92d759676efeb
kn9012 <kn9012@naver.com>
2025-08-13 13:58:01 +0900

    Initial commit

---
+++ heybro/src/main/java/com/heybro/heybro/HeybroApplication.java
@@ -0,0 +1,13 @@
+package com.heybro.heybro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeybroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeybroApplication.class, args);
+	}
+
+}
---
+++ heybro/src/test/java/com/heybro/heybro/HeybroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heybro.heybro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeybroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
