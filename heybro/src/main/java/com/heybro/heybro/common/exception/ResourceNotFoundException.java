325f3772385ebd7656772144efb6442a623fa1f0
kn9012 <kn9012@naver.com>
2025-08-16 23:06:48 +0900

    refactor: 라이브러리 정리

---
+++ heybro/src/main/java/com/heybro/heybro/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.heybro.heybro.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
