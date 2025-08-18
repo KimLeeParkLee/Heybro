9bfa9a7f715c77655cbe56e6a698f40b3d2dd62b
kn9012 <kn9012@naver.com>
2025-08-18 21:59:53 +0900

    chore: Spring Seuciry 인가 설정 수정

--- src/main/java/com/heybro/heybro/config/SecurityConfig.java
+++ src/main/java/com/heybro/heybro/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.heybro.heybro.common.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -50,7 +51,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/api/auth/**", "/api/oauth2/authorization").permitAll() // 로그인/인증 관련 경로는 모두 허용
-                        .requestMatchers("/api/users/**").permitAll() // 회원가입 및 이메일 검증
+                        .requestMatchers("/api/users").permitAll() // 회원가입 및 이메일 검증
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Swagger
                         .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
                 );
