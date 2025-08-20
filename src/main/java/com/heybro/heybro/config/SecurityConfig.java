3f716bee9cc6f07b4ebf1de7d5fecbe38bf9d2d3
kn9012 <kn9012@naver.com>
2025-08-20 13:49:50 +0900

    fix: Swagger UI 접근 경로 수정

--- src/main/java/com/heybro/heybro/config/SecurityConfig.java
+++ src/main/java/com/heybro/heybro/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/api/auth/**", "/login/oauth2/code/**").permitAll() // 로그인/인증 관련 경로는 모두 허용
                         .requestMatchers("/api/users").permitAll() // 회원가입 및 이메일 검증
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Swagger
+                        .requestMatchers("/api/v3/api-docs/**", "/api/swagger-ui/**", "/swagger-ui.html").permitAll() // Swagger
                         .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
                 );
 
