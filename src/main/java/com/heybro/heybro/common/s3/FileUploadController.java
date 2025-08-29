05db667c6133eddfdbe27f5fcaa4a03cda06ac6f
kn9012 <kn9012@naver.com>
2025-08-29 23:01:57 +0900

    feat: S3 파일 업로드 기능 추가

---
+++ src/main/java/com/heybro/heybro/common/s3/FileUploadController.java
@@ -0,0 +1,28 @@
+package com.heybro.heybro.common.s3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+public class FileUploadController {
+
+    private final S3UploadService s3UploadService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            String fileUrl = s3UploadService.saveFile(file);
+            return ResponseEntity.ok(fileUrl);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("파일 업로드에 실패했습니다: " + e.getMessage());
+        }
+    }
+}
