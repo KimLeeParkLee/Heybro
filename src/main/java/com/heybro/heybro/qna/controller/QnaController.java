c8ed40448c2ba2038564e69e978208efcdf79d2e
kn9012 <kn9012@naver.com>
2025-09-03 16:29:49 +0900

    fix: multipart 요청 처리 시 @RequestPart 대신 @RequestParam 사용

--- src/main/java/com/heybro/heybro/qna/controller/QnaController.java
+++ src/main/java/com/heybro/heybro/qna/controller/QnaController.java
@@ -12,9 +12,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -46,7 +44,7 @@ public class QnaController {
 
     @Operation(summary = "질문 작성")
     @PostMapping(consumes = {"multipart/form-data"})
-    public QuestionIdResponseDto createQuestion(@RequestPart("requestDto") QuestionRequestDto requestDto, @RequestPart("images") List<MultipartFile> images,
+    public QuestionIdResponseDto createQuestion(@RequestPart("requestDto") QuestionRequestDto requestDto, @RequestParam("images") List<MultipartFile> images,
                                                 @AuthenticationPrincipal UserDetails userDetails) {
 
         return qnaService.createQuestion(requestDto, images, userDetails.getUsername());
