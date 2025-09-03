b9e03eacb4120e24799dc9df7be14cb8e51ea2d0
kn9012 <kn9012@naver.com>
2025-09-03 18:22:06 +0900

    delete: Question get 쿼리 삭제

--- src/main/java/com/heybro/heybro/qna/repository/QuestionRepository.java
+++ src/main/java/com/heybro/heybro/qna/repository/QuestionRepository.java
@@ -1,8 +1,6 @@
 package com.heybro.heybro.qna.repository;
 
 import com.heybro.heybro.qna.domain.Question;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,8 +12,4 @@ public interface QuestionRepository extends JpaRepository<Question, Long>, Quest
     @Modifying
     @Query("update Question q set q.viewCount = q.viewCount + 1 where q.id = :id")
     void incrementViewCount(@Param("id") Long id);
-
-    @Query(value = "SELECT DISTINCT q FROM Question q LEFT JOIN FETCH q.answers",
-            countQuery = "SELECT count(q) FROM Question q")
-    Page<Question> findAllWithAnswers(Pageable pageable);
 }
