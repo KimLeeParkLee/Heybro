29edb34fd085fa7e32e734b16ecc42b0b90f938b
kn9012 <kn9012@naver.com>
2025-09-18 03:47:56 +0900

    fix: snake case 요청 시 @JsonProperty 추가

--- src/main/java/com/heybro/heybro/auth/dto/request/OAuth2LoginRequestDto.java
+++ src/main/java/com/heybro/heybro/auth/dto/request/OAuth2LoginRequestDto.java
@@ -1,5 +1,6 @@
 package com.heybro.heybro.auth.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,5 +12,6 @@ import lombok.NoArgsConstructor;
 @Schema(description = "소셜 로그인 요청 DTO")
 public class OAuth2LoginRequestDto {
     @Schema(description = "access token")
+    @JsonProperty("access_token")
     private String accessToken;
 }
