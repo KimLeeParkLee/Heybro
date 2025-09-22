1a2d335012ffebd051a98f913efea9bc65b384bb
kn9012 <kn9012@naver.com>
2025-09-22 21:38:39 +0900

    add: 상세 이미지 없었던 루틴 이미지 추가

--- src/main/java/com/heybro/heybro/routine/RoutineDataInitializer.java
+++ src/main/java/com/heybro/heybro/routine/RoutineDataInitializer.java
@@ -120,9 +120,9 @@ public class RoutineDataInitializer implements CommandLineRunner {
                 .iconImage("https://drive.google.com/file/d/1Q1bMGLr91Gp32nAEuL9kShLaOqQaEyX1/view?usp=sharing")
                 .tipList(createTips("세럼은 피부 고민에 맞는 제품을 선택하세요.", "여러 제품을 쓸 땐 점도가 묽은 것부터 바르세요."))
                 .elementList(Arrays.asList(
-                        RoutineElement.builder().step(1).name("세럼 펌핑하기").content("적당량의 세럼을 손에 펌핑합니다.").build(),
-                        RoutineElement.builder().step(2).name("얼굴에 펴 바르기").content("피부 중심에서 바깥쪽으로 부드럽게 발라주세요.").build(),
-                        RoutineElement.builder().step(3).name("손으로 감싸 흡수시키기").content("손바닥으로 얼굴을 감싸며 흡수시킵니다.").build()
+                        RoutineElement.builder().step(1).name("세럼 펌핑하기").content("적당량의 세럼을 손에 펌핑합니다.").detailImage("https://drive.google.com/uc?export=view&id=1RTfgJTZMZsdG_YocYk1S1fqxFyS77SMH").build(),
+                        RoutineElement.builder().step(2).name("얼굴에 펴 바르기").content("피부 중심에서 바깥쪽으로 부드럽게 발라주세요.").detailImage("https://drive.google.com/uc?export=view&id=1uF2XSn-dSZgWS0ErrPA0HZLajQibjo9k").build(),
+                        RoutineElement.builder().step(3).name("손으로 감싸 흡수시키기").content("손바닥으로 얼굴을 감싸며 흡수시킵니다.").detailImage("https://drive.google.com/uc?export=view&id=1i6Vas3l1rYYA98PImxilOPjKtNLH-XPR").build()
                 ))
                 .recommendedProductList(new ArrayList<>(Arrays.asList(
                         RecommendedProduct.builder()
@@ -144,9 +144,9 @@ public class RoutineDataInitializer implements CommandLineRunner {
                 .iconImage("https://drive.google.com/file/d/17G9fijMamsQy8XZVG2Rqp8XAvJMuo8DY/view?usp=sharing")
                 .tipList(createTips("목까지 함께 발라야 관리가 완성돼요.", "아침에는 가벼운 로션, 저녁엔 영양 크림이 적합합니다."))
                 .elementList(Arrays.asList(
-                        RoutineElement.builder().step(1).name("로션/크림 덜어내기").content("손등만한 크기의 양을 손바닥에 덜어주세요.").build(),
-                        RoutineElement.builder().step(2).name("얼굴에 펴 바르기").content("볼 → 이마 → 턱 순으로 바르며 고르게 펴 발라줍니다.").build(),
-                        RoutineElement.builder().step(3).name("손으로 눌러 흡수시키기").content("손바닥으로 얼굴을 지그시 눌러 흡수시킵니다.").build()
+                        RoutineElement.builder().step(1).name("로션/크림 덜어내기").content("손등만한 크기의 양을 손바닥에 덜어주세요.").detailImage("https://drive.google.com/uc?export=view&id=1gNbhLVf7Js7jDxZ-MisRwJwKylxLWkr").build(),
+                        RoutineElement.builder().step(2).name("얼굴에 펴 바르기").content("볼 → 이마 → 턱 순으로 바르며 고르게 펴 발라줍니다.").detailImage("https://drive.google.com/uc?export=view&id=1Loq7-WY9xiVv6Cib6he8fFXeWpRz5wSv").build(),
+                        RoutineElement.builder().step(3).name("손으로 눌러 흡수시키기").content("손바닥으로 얼굴을 지그시 눌러 흡수시킵니다.").detailImage("https://drive.google.com/uc?export=view&id=1KsdhJYe_etYSf-0i1Rkxx1pH5tM0Z-1B").build()
                 ))
                 .recommendedProductList(new ArrayList<>(Arrays.asList(
                         RecommendedProduct.builder()
@@ -216,8 +216,8 @@ public class RoutineDataInitializer implements CommandLineRunner {
                 .iconImage("https://drive.google.com/file/d/1dfr1pTE-eEpXPKyi3FEUeWl040cSmBBY/view?usp=sharing")
                 .tipList(createTips("칫솔질 전 가볍게 물로 헹구면 더 효과적입니다.", "치실이나 가글을 함께 사용하면 좋습니다."))
                 .elementList(Arrays.asList(
-                        RoutineElement.builder().step(1).name("식후 30분 뒤 양치").content("식사 후 30분 뒤 양치하세요.").build(),
-                        RoutineElement.builder().step(2).name("혀 닦아 마무리하기").content("혀까지 부드럽게 닦아 마무리합니다.").build()
+                        RoutineElement.builder().step(1).name("식후 30분 뒤 양치").content("식사 후 30분 뒤 양치하세요.").detailImage("https://drive.google.com/uc?export=view&id=1O4EW0cPIrO6GMEC_A1z5u9Omp0IOmq9A").build(),
+                        RoutineElement.builder().step(2).name("혀 닦아 마무리하기").content("혀까지 부드럽게 닦아 마무리합니다.").detailImage("https://drive.google.com/uc?export=view&id=1K1slpjj6_enGJPEOIgJ0GlFOPto9B8KA").build()
                 ))
                 .recommendedProductList(new ArrayList<>(Arrays.asList(
                         RecommendedProduct.builder()
@@ -239,8 +239,8 @@ public class RoutineDataInitializer implements CommandLineRunner {
                 .iconImage("https://drive.google.com/file/d/15YJ5DluvK9upjKenT4Hwlbw8T3caLnb-/view?usp=sharing")
                 .tipList(createTips("미스트는 미온수 성분이 든 제품이 자극이 적습니다.", "뿌린 뒤 휴지로 닦아내지 말고 흡수시켜주세요."))
                 .elementList(Arrays.asList(
-                        RoutineElement.builder().step(1).name("거리 두고 분사하기").content("얼굴에서 20cm 정도 거리를 두고 미스트를 분사하세요.").build(),
-                        RoutineElement.builder().step(2).name("두드려 흡수시키기").content("가볍게 손으로 두드려 흡수시킵니다.").build()
+                        RoutineElement.builder().step(1).name("거리 두고 분사하기").content("얼굴에서 20cm 정도 거리를 두고 미스트를 분사하세요.").detailImage("https://drive.google.com/uc?export=view&id=1f3AHq5ql4TEq183YgqNOgRlYAyFKMc5S").build(),
+                        RoutineElement.builder().step(2).name("두드려 흡수시키기").content("가볍게 손으로 두드려 흡수시킵니다.").detailImage("https://drive.google.com/uc?export=view&id=1fAd20GF1WSN96K_Ko3g8nzel57nMQ78v").build()
                 ))
                 .recommendedProductList(new ArrayList<>(Arrays.asList(
                         RecommendedProduct.builder()
@@ -284,8 +284,8 @@ public class RoutineDataInitializer implements CommandLineRunner {
                 .iconImage("https://drive.google.com/file/d/1zabKYnzrq7_SM4o-ZdiOSORnqWDQE6T5/view?usp=sharing")
                 .tipList(createTips("건조하기 쉬운 손등부터 챙기세요.", "끈적임이 적은 제형은 낮에 쓰기 좋아요."))
                 .elementList(Arrays.asList(
-                        RoutineElement.builder().step(1).name("손등에 덜어 문지르기").content("적당량을 손등에 짜서 양손으로 문질러 주세요.").build(),
-                        RoutineElement.builder().step(2).name("손가락과 큐티클에 바르기").content("손가락 사이와 손톱 큐티클까지 꼼꼼히 발라주세요.").build()
+                        RoutineElement.builder().step(1).name("손등에 덜어 문지르기").content("적당량을 손등에 짜서 양손으로 문질러 주세요.").detailImage("https://drive.google.com/uc?export=view&id=1Y14-VtzzrJn1kuvkxOIHEG6W2Y7-MNH7").build(),
+                        RoutineElement.builder().step(2).name("손가락과 큐티클에 바르기").content("손가락 사이와 손톱 큐티클까지 꼼꼼히 발라주세요.").detailImage("https://drive.google.com/uc?export=view&id=18Y5zse9EGdp-dn2IKc9Lh7f6IobkZtci").build()
                 ))
                 .recommendedProductList(new ArrayList<>(Arrays.asList(
                         RecommendedProduct.builder()
@@ -317,9 +317,9 @@ public class RoutineDataInitializer implements CommandLineRunner {
                 .iconImage("https://drive.google.com/file/d/1J6vOsP4wPlHgnD5guVENqYFCp1_IKM5V/view?usp=sharing")
                 .tipList(createTips("눈가, 콧방울은 특히 꼼꼼히 닦아주세요.", "세안 후 타올로 문지르지 말고 톡톡 두드리세요."))
                 .elementList(Arrays.asList(
-                        RoutineElement.builder().step(1).name("클렌징 오일로 녹이기").content("클렌징 오일로 메이크업과 노폐물을 녹여줍니다.").build(),
-                        RoutineElement.builder().step(2).name("폼 클렌저로 세안하기").content("폼 클렌저로 거품을 내어 피부결 방향으로 세안합니다.").build(),
-                        RoutineElement.builder().step(3).name("미온수로 헹궈내기").content("미지근한 물로 꼼꼼히 헹궈냅니다.").build()
+                        RoutineElement.builder().step(1).name("클렌징 오일로 녹이기").content("클렌징 오일로 메이크업과 노폐물을 녹여줍니다.").detailImage("https://drive.google.com/uc?export=view&id=1VM1u2BipMGZDowo3kYc3v-tWcWNdKB6r").build(),
+                        RoutineElement.builder().step(2).name("폼 클렌저로 세안하기").content("폼 클렌저로 거품을 내어 피부결 방향으로 세안합니다.").detailImage("https://drive.google.com/file/d/15ql1api0CqXVGslzbEo2Gv1R9wpgFrMZ/view?usp=drive_link").build(),
+                        RoutineElement.builder().step(3).name("미온수로 헹궈내기").content("미지근한 물로 꼼꼼히 헹궈냅니다.").detailImage("https://drive.google.com/file/d/1LY4HyRDf3K1qlGwNcrfY6D6BLJivURAy/view?usp=drive_link").build()
                 ))
                 .recommendedProductList(new ArrayList<>(Arrays.asList(
                         RecommendedProduct.builder()
@@ -341,9 +341,9 @@ public class RoutineDataInitializer implements CommandLineRunner {
                 .iconImage("https://drive.google.com/file/d/1mKJhBfjPlFgz89Ck3hNEQ5Cc0_0fT2BW/view?usp=sharing")
                 .tipList(createTips("겨울철엔 평소보다 조금 더 두껍게 발라주세요.", "피부 결 방향대로 바르는 게 흡수에 좋아요."))
                 .elementList(Arrays.asList(
-                        RoutineElement.builder().step(1).name("크림 덜어내기").content("손가락 끝에 크림을 소량씩 덜어냅니다.").build(),
-                        RoutineElement.builder().step(2).name("얼굴 부위별로 바르기").content("볼, 이마, 턱, 코 순으로 발라주세요.").build(),
-                        RoutineElement.builder().step(3).name("눌러서 흡수시키기").content("손바닥으로 지그시 눌러 흡수시킵니다.").build()
+                        RoutineElement.builder().step(1).name("크림 덜어내기").content("손가락 끝에 크림을 소량씩 덜어냅니다.").detailImage("https://drive.google.com/file/d/1FKXTI_lB2JWSUjZfaIJe9b4HAfFxVwdt/view?usp=drive_link").build(),
+                        RoutineElement.builder().step(2).name("얼굴 부위별로 바르기").content("볼, 이마, 턱, 코 순으로 발라주세요.").detailImage("https://drive.google.com/file/d/1Iw0lwkdhT2bE1C612-Dri7jn9tIIQLCR/view?usp=drive_link").build(),
+                        RoutineElement.builder().step(3).name("눌러서 흡수시키기").content("손바닥으로 지그시 눌러 흡수시킵니다.").detailImage("https://drive.google.com/file/d/1wDLGfIBMFjC52FNgn1KCM89d6LIuZHDE/view?usp=drive_link").build()
                 ))
                 .recommendedProductList(new ArrayList<>(Arrays.asList(
                         RecommendedProduct.builder()
@@ -365,9 +365,9 @@ public class RoutineDataInitializer implements CommandLineRunner {
                 .iconImage("https://drive.google.com/file/d/18mX5LE-Eox8Jg9qamyIIF5OwOZDH2QbD/view?usp=sharing")
                 .tipList(createTips("자르기보다는 갈아서 관리하면 손상이 적습니다.", "취침 전 관리하면 흡수가 잘돼요."))
                 .elementList(Arrays.asList(
-                        RoutineElement.builder().step(1).name("길이 정돈하기").content("손톱깎이나 파일로 길이를 정돈합니다.").build(),
-                        RoutineElement.builder().step(2).name("오일/로션으로 보습").content("큐티클 오일이나 로션으로 보습합니다.").build(),
-                        RoutineElement.builder().step(3).name("코팅제로 보호하기").content("매니큐어/투명 코팅제로 보호막을 씌워줍니다.").build()
+                        RoutineElement.builder().step(1).name("길이 정돈하기").content("손톱깎이나 파일로 길이를 정돈합니다.").detailImage("https://ibb.co/gFv4rCt4").build(),
+                        RoutineElement.builder().step(2).name("오일/로션으로 보습").content("큐티클 오일이나 로션으로 보습합니다.").detailImage("https://ibb.co/G4H09374").build(),
+                        RoutineElement.builder().step(3).name("코팅제로 보호하기").content("매니큐어/투명 코팅제로 보호막을 씌워줍니다.").detailImage("https://drive.google.com/file/d/1R-hpkifprg0Zvk5_Z-HI7xh0uP85Rj0A/view?usp=drive_link").build()
                 ))
                 .recommendedProductList(new ArrayList<>(Arrays.asList(
                         RecommendedProduct.builder()
@@ -389,9 +389,9 @@ public class RoutineDataInitializer implements CommandLineRunner {
                 .iconImage("https://drive.google.com/file/d/1j5NQU-lNrf7nW2dCRaA6Ul03ni-Afk6p/view?usp=sharing")
                 .tipList(createTips("스트레칭은 10분 이내가 적당합니다.", "무리하지 말고 호흡에 맞춰 하세요."))
                 .elementList(Arrays.asList(
-                        RoutineElement.builder().step(1).name("목과 어깨 풀기").content("가볍게 목과 어깨를 돌려 풀어줍니다.").build(),
-                        RoutineElement.builder().step(2).name("다리 근육 늘리기").content("허리를 숙여 다리 근육을 늘려줍니다.").build(),
-                        RoutineElement.builder().step(3).name("전신 스트레칭").content("누운 상태에서 전신 스트레칭을 합니다.").build()
+                        RoutineElement.builder().step(1).name("목과 어깨 풀기").content("가볍게 목과 어깨를 돌려 풀어줍니다.").detailImage("https://ibb.co/rPPkqs9").build(),
+                        RoutineElement.builder().step(2).name("다리 근육 늘리기").content("허리를 숙여 다리 근육을 늘려줍니다.").detailImage("https://ibb.co/2Y1tKNtk").build(),
+                        RoutineElement.builder().step(3).name("전신 스트레칭").content("누운 상태에서 전신 스트레칭을 합니다.").detailImage("https://ibb.co/60XBNdd9").build()
                 ))
                 .build());
         routines.add(Routine.builder()
@@ -400,8 +400,8 @@ public class RoutineDataInitializer implements CommandLineRunner {
                 .iconImage("https://drive.google.com/file/d/1BhJP1boulSgaUM9sElEAceUA604yK1Zh/view?usp=sharing")
                 .tipList(createTips("눈가 전용 크림을 함께 쓰면 효과가 좋아요.", "너무 강하게 누르지 않도록 주의하세요."))
                 .elementList(Arrays.asList(
-                        RoutineElement.builder().step(1).name("눈썹 뼈 누르기").content("눈썹 아래 뼈를 지그시 눌러줍니다.").build(),
-                        RoutineElement.builder().step(2).name("눈꼬리/관자놀이 마사지").content("눈꼬리와 관자놀이를 원을 그리며 마사지합니다.").build()
+                        RoutineElement.builder().step(1).name("눈썹 뼈 누르기").content("눈썹 아래 뼈를 지그시 눌러줍니다.").detailImage("https://ibb.co/PG888fWH").build(),
+                        RoutineElement.builder().step(2).name("눈꼬리/관자놀이 마사지").content("눈꼬리와 관자놀이를 원을 그리며 마사지합니다.").detailImage("https://ibb.co/7NzP5xNZ").build()
                 ))
                 .recommendedProductList(new ArrayList<>(Arrays.asList(
                         RecommendedProduct.builder()
