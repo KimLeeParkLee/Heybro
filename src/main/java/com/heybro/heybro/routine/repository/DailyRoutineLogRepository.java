f4814961841dc1c2fa46ec202743c7eee2b3dc59
kn9012 <kn9012@naver.com>
2025-09-29 01:00:04 +0900

    fix: 루틴 로그 생성 시 중복 데이터 생성하여 비과적 락 적용

--- src/main/java/com/heybro/heybro/routine/repository/DailyRoutineLogRepository.java
+++ src/main/java/com/heybro/heybro/routine/repository/DailyRoutineLogRepository.java
@@ -3,7 +3,9 @@ package com.heybro.heybro.routine.repository;
 import com.heybro.heybro.routine.domain.DailyRoutineLog;
 import com.heybro.heybro.routine.domain.Routine;
 import com.heybro.heybro.user.domain.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -18,6 +20,7 @@ public interface DailyRoutineLogRepository extends JpaRepository<DailyRoutineLog
 
     List<DailyRoutineLog> findAllByUserAndTaskDate(User user, LocalDate taskDate);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     DailyRoutineLog findByUserAndRoutineAndTaskDate(User user, Routine routine, LocalDate taskDate);
 
     @Query("select d from DailyRoutineLog d join fetch d.routine where d.user = :user and d.taskDate between :startDate and :endDate")
