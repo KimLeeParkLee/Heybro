89a25d03f2664a9b1f44f4250bd1688b3dba280c
kn9012 <kn9012@naver.com>
2025-09-29 01:02:09 +0900

    fix: 서버 시간과 실제 시간의 불일치 문제 해결

--- src/main/java/com/heybro/heybro/HeybroApplication.java
+++ src/main/java/com/heybro/heybro/HeybroApplication.java
@@ -1,12 +1,19 @@
 package com.heybro.heybro;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class HeybroApplication {
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HeybroApplication.class, args);
