9fd3c935618c520d9069685952919a6df0581383
kn9012 <kn9012@naver.com>
2025-10-26 13:58:45 +0900

    fix: Prometheus 메트릭 엔드포인트 접근 허용

--- src/main/java/com/heybro/heybro/config/SecurityConfig.java
+++ src/main/java/com/heybro/heybro/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/auth/**", "/login/oauth2/code/**").permitAll() // 로그인/인증 관련 경로는 모두 허용
                         .requestMatchers("/users", "/users/check-nickname").permitAll() // 회원가입 및 이메일/닉네임 검증
+                        .requestMatchers("/api/actuator/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Swagger
                         .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
                 );
